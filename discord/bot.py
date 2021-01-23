bf742f62b43d088ba256413bee923bb1b50d3e9b
Thomas <tdevries4i@gmail.com>
2021-01-23 16:11:05 -0500

    feat: creating a functional bot

--- discord/bot.py
+++ discord/bot.py
@@ -1,6 +1,21 @@
 import configparser
+import discord
 
 config = configparser.ConfigParser()
 config.read("secrets.ini")
 
-print(config['DISCORD']['user'])
+
+class NickBot(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+client = NickBot()
+client.run(config['DISCORD']['bot_token'])
