45f72004b2ffde8ad2d16c3f7afe825f736d0295
Naseus <tdevries4i@gmail.com>
2021-02-26 21:44:51 -0500

    feat: Bad math can now process staments

--- discord_bot/puzzles/BadMath.py
+++ discord_bot/puzzles/BadMath.py
@@ -6,30 +6,57 @@ class BadMath(Puzzle):
 
     def check_answer(self, s):
         """
-        WARNING: this one takes s as an int
+        WARNING: this one takes s as a string
         """
-        return self.eval_stmt(s) == 29
+        return self.eval_stmt(s) == 100
 
     def get_response(self, s):
-        for char in s:
-            if char in '0123456789':
-                return 'ERROR:NaN'
-        return self.eval_stmt(s)
+        return self.eval_stmt()
 
     def eval_stmt(self, s):
-        self.clean_stmt(s)
+        nums = ['trash', 'dogshit', 'garbage']
+        lst = []
+        rtn =''
+        for char in s:
+            if char in '0123456789':
+                return 'ARITHMATIC ERROR'
+        s = self.clean_stmt(s)
         try:
-            eval(s)
+            i  =  eval(s)
+            if i == 0:
+                return nums[0]
+            if i < 0:
+                rtn += '-'
+                i *= -1
+            while i:
+                i, r = divmod(i, 3)
+                lst.append(nums[r])
+            rtn += ''.join(reversed(lst))
+            return rtn
         except NameError:
-            return 'ERROR:NaN'
+            return 'ARITHMATIC ERROR'
 
     def clean_stmt(self, s):
-        lst = re.split(r'(trash|dogshit|garbage|+|-|*|/)', s)
-        lst = filter(None, lst)
-        lst = filter(lambda x: x != " ", lst)
-        convert_nums(lst)
+        num_lst = []
+        rtn = ''
+        lst = re.split(r'(trash|dogshit|garbage|\+|-|\*|\/)', s)
+        print(lst)
+        lst = list(filter(None, lst))
+        lst = list(filter(lambda x: x != " ", lst))
+        print(lst)
+        for tok in lst:
+            if tok == 'trash' or tok == 'dogshit' or  tok == 'garbage':
+                num_lst.append(tok)
+            else:
+                if(num_lst):
+                    rtn += str(self.convert_to_num(num_lst))
+                rtn += str(tok)
+                num_lst = []
+        if(num_lst):
+            rtn += str(self.convert_to_num(num_lst))
+        return rtn
 
-    def convert_num(self, lst):
+    def convert_to_num(self, lst):
         rtn = 0
         nums = {
                 'trash': 0,
--- discord_bot/puzzles/tests/test_bad_math.py
+++ discord_bot/puzzles/tests/test_bad_math.py
@@ -6,6 +6,30 @@ class TestBadMath(unittest.TestCase):
     def setUp(self):
         self.my_math = BadMath("N/A")
 
+    # Test to make sure the coverter is properly converting base 3 numbers
     def test_converter(self):
         lst = ["dogshit", "trash", "trash"]
-        assert self.my_math.convert_num(lst) == 9
+        assert self.my_math.convert_to_num(lst) == 9
+
+    # Test that the clean_stmt function can run for a properly formed package
+    def test_clean_statment(self):
+        s = "dogshittrashgarbage + dogshit - garbage * garbage"
+        s = self.my_math.clean_stmt(s)
+        assert s == '11+1-2*2'
+
+    # Test that clean_function does not crash on a malformed package
+    def test_clean_statment_on_bad_string(self):
+        s = "dogshittrahgarbage + dogsht - garbage * garbage"
+        s = self.my_math.clean_stmt(s)
+
+    # Test that +- works for eval
+    def test_eval_plusminus(self):
+        s = "dogshittrashgarbage + -dogshit - garbage * garbage"
+        i = self.my_math.eval_stmt(s)
+        assert i == 'garbagetrash'
+
+    # Test that eval works for negitives
+    def test_eval_negitive(self):
+        s = "-dogshittrashgarbage + -dogshit - garbage * garbage"
+        i = self.my_math.eval_stmt(s)
+        assert i == '-dogshitgarbagedogshit'
