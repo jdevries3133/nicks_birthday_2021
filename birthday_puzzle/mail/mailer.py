b23859470c2878356a7b8b1c3fa40bf69ef9ed2f
Jack DeVries <jdevries3133@gmail.com>
2021-03-12 00:00:00 +0000

    temp: progress on riddler

--- birthday_puzzle/mail/mailer.py
+++ birthday_puzzle/mail/mailer.py
@@ -2,11 +2,19 @@
 Wrapper class for ClickSend API
 """
 
-# import json
-# from base64 import b64encode
-# from pathlib import Path
+import json
+from base64 import b64encode
+from pathlib import Path
 
-# import requests
+import requests
+
+from .riddles import Riddler
+from ..email_puzzles.emailer import EmailBot
 
 
-# TODO: implement me
+class Mailer(EmailBot):
+
+    def __init__(self):
+        self.ridl = Riddler()
+
+
