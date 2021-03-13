891f3554b3cf4526dbcc1eb023d74dc8f96e3487
Kate Tell <katell828@gmail.com>
2021-03-12 21:25:59 -0500

    feat: clicksend api wrapper begin; mailer progress

--- birthday_puzzle/mail/mailer.py
+++ birthday_puzzle/mail/mailer.py
@@ -2,11 +2,22 @@
 Wrapper class for ClickSend API
 """
 
-# import json
-# from base64 import b64encode
-# from pathlib import Path
+import json
+from base64 import b64encode
+from pathlib import Path
 
-# import requests
+import requests
 
+from .clicksend import ClickSend
+from .riddles import Riddler
+from ..email_puzzles.emailer import EmailBot
 
-# TODO: implement me
+with open(Path(__file__).parent, 'secrets.json') as jsonf:
+    secrets = json.load(jsonf)
+
+class Mailer(EmailBot):
+    def __init__(self):
+        self.clicksend = ClickSend(
+            username=secrets['clicksend']['username'],
+            api_key=secrets['clicksend']['API_KEY']
+        )
